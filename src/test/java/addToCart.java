fb5dbb212083a8ad45b1a0f18c992b0fbcb4f289
Sandzhlavi <osandzlavi@gmail.com>
2024-06-20 21:32:43 +0300

    Add to Cart

--- src/test/java/addToCart.java
+++ src/test/java/addToCart.java
@@ -1,3 +1,5 @@
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
@@ -7,8 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-import static org.junit.Assert.assertEquals;
-import static org.openqa.selenium.devtools.v123.dom.DOM.querySelector;
+import static org.junit.Assert.*;
 
 public class addToCart {
   WebDriver driver;
@@ -16,36 +17,59 @@ public class addToCart {
 
   @Before
   public void setup() {
-    driver = new ChromeDriver();
+        driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    wait = new WebDriverWait(driver, Duration.ofSeconds(20));
   }
 
   @Test
-
   public void Cart() {
     driver.get("https://theconnectedshop.com/");
-    driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
-    wait=new WebDriverWait(driver,Duration.ofSeconds(10));
-    driver.findElement(By.className("shopify-pc__banner__btn-accept")).click();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(35));
 
 
-    WebElement search = driver.findElement(By.xpath("//*[@id=\"section-header\"]/div/div[1]/nav/ul/li[4]/a"));
+    WebElement acceptButton = driver.findElement(By.xpath("//*[@id=\"section-header\"]/div/div[1]/nav/ul/li[4]/a"));
+    acceptButton.click();
+
+
+    WebElement search = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"section-header\"]/div/div[1]/nav/ul/li[4]/a")));
     search.click();
-    WebElement products = driver.findElement(By.xpath("//*[@id=\"section-list-collections-template\"]/div/a[1]/div/div[2]/header/div/span"));
+
+
+    WebElement products = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"section-list-collections-template\"]/div/a[1]/div/div[2]/header/div/span")));
     products.click();
+    WebElement productTitle = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h2[@class='ProductItem__Title Heading']/a[@href='/collections/connected-home/products/fingerprint-door-lock']")));
 
-    WebElement smartDoorLock = driver.findElement(By.xpath("//*[@id=\"shopify-section-collection-template\"]/section/div[4]/div[1]/div[2]/div[1]/div/div[3]/div/div/div/h2/a"));
-    WebElement productTitle = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h2[@class='ProductItem__Title Heading']/a[@href='/collections/connected-home/products/smart-door-lock-slim']")));
-    if (productTitle != null) {
-      ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", productTitle);
+    WebElement fingerPrintDoorLock = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"shopify-section-collection-template\"]/section/div[4]/div[1]/div[2]/div[1]/div/div[3]/div/div/div/h2/a")));
+    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", fingerPrintDoorLock);
+    fingerPrintDoorLock = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"shopify-section-collection-template\"]/section/div[4]/div[1]/div[2]/div[1]/div/div[3]/div/div/div/h2/a")));
+    ((JavascriptExecutor) driver).executeScript("arguments[0].click();", fingerPrintDoorLock);
+
+   WebElement addToCartButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"product_form_6198418636972\"]/button")));
+      WebElement addToCartButton1 = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"product_form_6198418636972\"]/button")));
+   addToCartButton.click();
 
 
-      productTitle = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//h2[@class='ProductItem__Title Heading']/a[@href='/collections/connected-home/products/smart-door-lock-slim']")));
-      ((JavascriptExecutor) driver).executeScript("arguments[0].click();", productTitle);
+   if (addToCartButton.isDisplayed()) {
 
-      assertEquals("Smart Door Lock Slim", productTitle.getText());
-      assertEquals("/collections/connected-home/products/smart-door-lock-slim", productTitle.getAttribute("href"));
+          addToCartButton.click();
+          Assert.assertTrue(addToCartButton.isDisplayed());
+          Assert.assertFalse(addToCartButton.isEnabled());
+          assertEquals("addToCartButton is inactive after click", false, addToCartButton.isEnabled());
+          assertEquals("addToCartButton is displayed",true,addToCartButton.isDisplayed());
+          assertNotNull (addToCartButton);
+          assertNotSame(addToCartButton,addToCartButton1);
+           assertEquals("submit", addToCartButton.getAttribute("type"));
+          assertEquals("add-to-cart", addToCartButton.getAttribute("data-action"));
+         String spanText = addToCartButton.getTagName();
+         assertEquals("button",spanText);
+
+
+       System.out.println("Product title was added to Cart");
     } else {
-      System.out.println("Product title with link not found");
+      System.out.println("Product title wasn't added to Cart ");
     }
-  } }
+  }
+
+
+  }
